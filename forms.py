013f063a42e26e46ae23aaed6bf5a25d0617f1e1
repl.it user <replituser@example.com>
2022-11-13 20:57:07 +0200

    Website project

--- forms.py
+++ forms.py
@@ -1,29 +1,57 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, SelectField, IntegerField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
 
 ##WTForm
-class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
+class CreateReviewForm(FlaskForm):
+    subject = SelectField("שם המוערך", validators=[DataRequired()])
+    keep_pts = StringField("נקודות לשימור", validators=[DataRequired()])
+    improve_pts = StringField("נקודות לשיפור", validators=[DataRequired()])
+    op_level = SelectField("רמת הפעלה", choices=[(4, '4'), (5, '5'),(6, '6'),(7, '7'),(8, '8'),(9, '9'),(10, '10')], validators=[DataRequired()])
+    knowledge_level = SelectField("רמת ידע מקצועי", choices=[(4, '4'), (5, '5'),(6, '6'),(7, '7'),(8, '8'),(9, '9'),(10, '10')], validators=[DataRequired()])
+    submit = SubmitField("הוסף חוות דעת")
 
+class EditReviewForm(FlaskForm):
+    keep_pts = StringField("נקודות לשימור", validators=[DataRequired()])
+    improve_pts = StringField("נקודות לשיפור", validators=[DataRequired()])
+    op_level = SelectField("רמת הפעלה", choices=[(4, '4'), (5, '5'),(6, '6'),(7, '7'),(8, '8'),(9, '9'),(10, '10')], validators=[DataRequired()])
+    knowledge_level = SelectField("רמת ידע מקצועי", choices=[(4, '4'), (5, '5'),(6, '6'),(7, '7'),(8, '8'),(9, '9'),(10, '10')], validators=[DataRequired()])
+    submit = SubmitField("ערוך חוות דעת")
 
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    name = StringField("Name", validators=[DataRequired()])
-    submit = SubmitField("Sign Me Up!")
+    id = IntegerField("מספר אישי", validators=[DataRequired()])
+    password = PasswordField("סיסמה", validators=[DataRequired()])
+    name = StringField("שם מלא", validators=[DataRequired()])
+    op_flight_time = StringField("שעות טיסה מבצעיות", validators=[DataRequired()])
+    tr_flight_time = StringField("שעות טיסת אימון", validators=[DataRequired()])
+    last_flight_date = StringField("תאריך טיסה אחרונה", validators=[DataRequired()])
+    qualified = StringField("כשירות", validators=[DataRequired()])
+    madrat = StringField("מדרט", validators=[DataRequired()])
+    coach = StringField("מאמן", validators=[DataRequired()])
+
+    submit = SubmitField("הוסף משתמש")
+
+class EditUserForm(FlaskForm):
+    name = StringField("שם מלא", validators=[DataRequired()])
+    op_flight_time = StringField("שעות טיסה מבצעיות", validators=[DataRequired()])
+    tr_flight_time = StringField("שעות טיסת אימון", validators=[DataRequired()])
+    last_flight_date = StringField("תאריך טיסה אחרונה", validators=[DataRequired()])
+    qualified = StringField("כשירות", validators=[DataRequired()])
+    madrat = StringField("מדרט", validators=[DataRequired()])
+    coach = StringField("מאמן", validators=[DataRequired()])
+    submit = SubmitField("השלם עריכה")
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Let Me In!")
+    id = StringField("מספר אישי", validators=[DataRequired()])
+    password = PasswordField("סיסמה", validators=[DataRequired()])
+    submit = SubmitField("התחבר")
+
+class Search_review(FlaskForm):
+    name = StringField("הקלד את שם המשתמש עליו תרצה לקרוא חוות דעת")
+    submit = SubmitField("חפש")
 
 
 class CommentForm(FlaskForm):
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
+from forms import LoginForm, RegisterForm, CreateReviewForm, CommentForm, EditUserForm, Search_review, EditReviewForm
 from flask_gravatar import Gravatar
+from werkzeug.datastructures import MultiDict
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -16,8 +17,12 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
+
+
+
+
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
@@ -30,39 +35,46 @@ def load_user(user_id):
 
 
 ##CONFIGURE TABLE
+
+
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(100))
-    posts = relationship("BlogPost", back_populates="author")
-    comments = relationship("Comment", back_populates="comment_author")
+    password = db.Column(db.String(1000), nullable=False)
+    name = db.Column(db.String(1000), nullable=False)
+    op_flight_time = db.Column(db.String(1000), nullable=False)
+    tr_flight_time = db.Column(db.String(1000), nullable=False)
+    last_flight_date = db.Column(db.String(1000), nullable=False)
+    qualified = db.Column(db.String(1000), nullable=False)
+    madrat = db.Column(db.String(1000), nullable=False)
+    coach = db.Column(db.String(1000), nullable=False)
+    reviews = relationship("Review", back_populates="author")
 
 
-class BlogPost(db.Model):
-    __tablename__ = "blog_posts"
+class Review(db.Model):
+    __tablename__ = "reviews"
     id = db.Column(db.Integer, primary_key=True)
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    author = relationship("User", back_populates="posts")
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.String(250), nullable=False)
-    comments = relationship("Comment", back_populates="parent_post")
+    author = relationship("User", back_populates="reviews")
+    author_name = db.Column(db.String(250), nullable=False)
+    subject = db.Column(db.String(250), nullable=False)
+    keep_pts = db.Column(db.Text, nullable=False)
+    improve_pts = db.Column(db.Text, nullable=False)
+    op_level = db.Column(db.Integer, nullable=False)
+    knowledge_level = db.Column(db.Integer, nullable=False)
 
 
-class Comment(db.Model):
-    __tablename__ = "comments"
-    id = db.Column(db.Integer, primary_key=True)
-    post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
-    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    parent_post = relationship("BlogPost", back_populates="comments")
-    comment_author = relationship("User", back_populates="comments")
-    text = db.Column(db.Text, nullable=False)
-db.create_all()
-
+# class Comment(db.Model):
+#     __tablename__ = "comments"
+#     id = db.Column(db.Integer, primary_key=True)
+#     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
+#     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+#     parent_post = relationship("BlogPost", back_populates="comments")
+#     comment_author = relationship("User", back_populates="comments")
+#     text = db.Column(db.Text, nullable=False)
+# db.create_all()
+#
 
 def admin_only(f):
     @wraps(f)
@@ -75,35 +87,42 @@ def admin_only(f):
 
 @app.route('/')
 def get_all_posts():
-    posts = BlogPost.query.all()
-    return render_template("index.html", all_posts=posts, current_user=current_user)
+    if not current_user.is_authenticated:
+        return redirect(url_for("login"))
+    # posts = BlogPost.query.all()
+    return render_template("index.html", current_user=current_user)
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-
-        if User.query.filter_by(email=form.email.data).first():
-            print(User.query.filter_by(email=form.email.data).first())
+        if User.query.filter_by(id=form.id.data).first():
+            print(User.query.filter_by(id=form.id.data).first())
             #User already exists
-            flash("You've already signed up with that email, log in instead!")
+            flash("משתמש כבר קיים! אנא התחבר")
             return redirect(url_for('login'))
 
-        hash_and_salted_password = generate_password_hash(
-            form.password.data,
-            method='pbkdf2:sha256',
-            salt_length=8
-        )
+        # hash_and_salted_password = generate_password_hash(
+        #     form.password.data,
+        #     method='pbkdf2:sha256',
+        #     salt_length=8
+        # )
         new_user = User(
-            email=form.email.data,
+            id=form.id.data,
             name=form.name.data,
-            password=hash_and_salted_password,
+            password=form.password.data,
+            op_flight_time=form.op_flight_time.data,
+            tr_flight_time=form.tr_flight_time.data,
+            last_flight_date=form.last_flight_date.data,
+            qualified=form.qualified.data,
+            madrat=form.madrat.data,
+            coach=form.coach.data,
         )
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
-        return redirect(url_for("get_all_posts"))
+        return render_template("index.html", current_user=current_user)
 
     return render_template("register.html", form=form, current_user=current_user)
 
@@ -112,16 +131,15 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        email = form.email.data
+        id = form.id.data
         password = form.password.data
-
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(id=id).first()
         # Email doesn't exist or password incorrect.
         if not user:
-            flash("That email does not exist, please try again.")
+            flash("מספר אישי שגוי")
             return redirect(url_for('login'))
-        elif not check_password_hash(user.password, password):
-            flash('Password incorrect, please try again.')
+        elif not user.password == password:
+            flash('סיסמה לא נכונה')
             return redirect(url_for('login'))
         else:
             login_user(user)
@@ -139,7 +157,6 @@ def logout():
 def show_post(post_id):
     form = CommentForm()
     requested_post = BlogPost.query.get(post_id)
-
     if form.validate_on_submit():
         if not current_user.is_authenticated:
             flash("You need to login or register to comment.")
@@ -166,57 +183,131 @@ def contact():
     return render_template("contact.html", current_user=current_user)
 
 
-@app.route("/new-post", methods=["GET", "POST"])
-@admin_only
-def add_new_post():
-    form = CreatePostForm()
+@app.route("/new-review", methods=["GET", "POST"])
+def add_new_review():
+    names = []
+    for user in User.query.all():
+        if user.name != "מנהל" and user.name!= current_user.name:
+            names.append(user.name)
+    names.sort()
+    form = CreateReviewForm()
+    form.subject.choices=names
     if form.validate_on_submit():
-        new_post = BlogPost(
-            title=form.title.data,
-            subtitle=form.subtitle.data,
-            body=form.body.data,
-            img_url=form.img_url.data,
+        new_review = Review(
+            subject=form.subject.data,
+            keep_pts=form.keep_pts.data,
+            improve_pts=form.improve_pts.data,
+            op_level=form.op_level.data,
+            knowledge_level=form.knowledge_level.data,
             author=current_user,
+            author_name=current_user.name,
             date=date.today().strftime("%B %d, %Y")
         )
-        db.session.add(new_post)
+        db.session.add(new_review)
         db.session.commit()
         return redirect(url_for("get_all_posts"))
 
     return render_template("make-post.html", form=form, current_user=current_user)
 
 
-
-
-@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+@app.route('/panel')
 @admin_only
-def edit_post(post_id):
-    post = BlogPost.query.get(post_id)
-    edit_form = CreatePostForm(
-        title=post.title,
-        subtitle=post.subtitle,
-        img_url=post.img_url,
-        author=current_user,
-        body=post.body
+def manage():
+    users = User.query.all()
+    return render_template('panel.html', users=users)
+
+@app.route("/edit-user/<int:user_id>", methods=["GET", "POST"])
+@admin_only
+def edit_user(user_id):
+    user = User.query.get(user_id)
+    edit_form = EditUserForm(
+        name=user.name,
+        op_flight_time=user.op_flight_time,
+        tr_flight_time=user.tr_flight_time,
+        last_flight_date=user.last_flight_date,
+        qualified=user.qualified,
+        madrat=user.madrat,
+        coach=user.coach
     )
     if edit_form.validate_on_submit():
-        post.title = edit_form.title.data
-        post.subtitle = edit_form.subtitle.data
-        post.img_url = edit_form.img_url.data
-        post.body = edit_form.body.data
+        user.name = edit_form.name.data
+        user.op_flight_time = edit_form.op_flight_time.data
+        user.tr_flight_time = edit_form.tr_flight_time.data
+        user.last_flight_date = edit_form.last_flight_date.data
+        user.qualified = edit_form.qualified.data
+        user.madrat = edit_form.madrat.data
+        user.coach = edit_form.coach.data
         db.session.commit()
-        return redirect(url_for("show_post", post_id=post.id))
+        return redirect(url_for("manage"))
+    return render_template("register.html", form=edit_form,is_edit=True, current_user=current_user)
 
+
+@app.route("/delete/<int:user_id>")
+@admin_only
+def delete_post(user_id):
+    user_to_delete = User.query.get(user_id)
+    db.session.delete(user_to_delete)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
+
+@app.route('/reviews-finder', methods=["GET", "POST"])
+@admin_only
+def search_reviews():
+    form = Search_review()
+    if form.validate_on_submit():
+        name = form.name.data
+        if name == "":
+            return redirect(url_for('show_reviews', user_id=0))
+        else:
+            user = User.query.filter_by(name=form.name.data).first()
+            if user:
+                return redirect(url_for('show_reviews', user_id=user.id))
+            else:
+                flash("לא נמצא משתמש")
+                return render_template("search.html", form=form)
+    return render_template("search.html", form=form)
+
+
+@app.route("/reviews/<int:user_id>", methods=["GET", "POST"])
+@admin_only
+def show_reviews(user_id):
+    if user_id == 0:
+        reviews = Review.query.all()
+        return render_template('reviews.html', reviews=reviews, user_id=user_id)
+    user = User.query.filter_by(id=user_id).first()
+    reviews = Review.query.filter_by(subject=user.name).all()
+    return render_template('reviews.html', reviews=reviews, user_id=user_id)
+
+
+@app.route("/edit-review/<int:review_id>", methods=["GET", "POST"])
+@admin_only
+def edit_review(review_id):
+    review = Review.query.get(review_id)
+    id = User.query.filter_by(name=review.subject).first().id
+    edit_form = EditReviewForm(
+        keep_pts=review.keep_pts,
+        improve_pts=review.improve_pts,
+        op_level=review.op_level,
+        knowledge_level=review.knowledge_level
+    )
+    if edit_form.validate_on_submit():
+        review.keep_pts = edit_form.keep_pts.data
+        review.improve_pts = edit_form.improve_pts.data
+        review.op_level = edit_form.op_level.data
+        review.knowledge_level = edit_form.knowledge_level.data
+        db.session.commit()
+        return redirect(url_for("search_reviews"))
     return render_template("make-post.html", form=edit_form, is_edit=True, current_user=current_user)
 
 
-@app.route("/delete/<int:post_id>")
+@app.route("/delete-review/<int:review_id>")
 @admin_only
-def delete_post(post_id):
-    post_to_delete = BlogPost.query.get(post_id)
-    db.session.delete(post_to_delete)
+def delete_review(review_id):
+    review_to_delete = Review.query.get(review_id)
+    id = User.query.filter_by(name=review_to_delete.subject).first().id
+    db.session.delete(review_to_delete)
     db.session.commit()
-    return redirect(url_for('get_all_posts'))
+    return redirect(url_for("search_reviews"))
 
 
 if __name__ == "__main__":
